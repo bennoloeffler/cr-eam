472ba05f1ff0980f666e1bda441a38200166e4d2
bennoloeffler <benno.loeffler@gmx.de>
2022-09-20 20:29:59 +0200

    initial

--- src/cr_eam/core.clj
+++ src/cr_eam/core.clj
@@ -1,7 +1,15 @@
 (ns cr-eam.core
+  (:require [ring.adapter.jetty :as jetty])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "2 BEL: Hello, World!"))
+  (println "2 BEL: Hello, World!")
+  (jetty/run-jetty (fn [req] {:status 200 :body "BELHello" :headers {}})  ;; a really basic handler
+                   {:port (System/getenv "PORT")     ;; listen on port 3001
+                    :join? false})
+  (println "main thread finished"))
+
+   ;; don't block the main thread)
+
