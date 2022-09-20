a1ef146583b419a5dabc6860f055e8459b235328
bennoloeffler <benno.loeffler@gmx.de>
2022-09-20 20:32:39 +0200

    initial

--- src/cr_eam/core.clj
+++ src/cr_eam/core.clj
@@ -7,7 +7,7 @@
   [& args]
   (println "2 BEL: Hello, World!")
   (jetty/run-jetty (fn [req] {:status 200 :body "BELHello" :headers {}})  ;; a really basic handler
-                   {:port (System/getenv "PORT")     ;; listen on port 3001
+                   {:port (Integer/parseInt (System/getenv "PORT"))     ;; listen on port 3001
                     :join? false})
   (println "main thread finished"))
 
