bf038ec13c0882430fb8d3ed6cd0a86d8cae828e
bennoloeffler <benno.loeffler@gmx.de>
2022-09-24 12:39:45 +0200

    initial

--- src/cr_eam/core.clj
+++ src/cr_eam/core.clj
@@ -1,8 +1,18 @@
 (ns cr-eam.core
   (:require [ring.adapter.jetty :as jetty])
   (:gen-class))
+
+;; https://ericnormand.me/guide/clojure-web-tutorial
+
+(def server (atom nil))
+
 (def counter (atom 0M))
 (def last-time (atom 0))
+
+(defonce count-down
+         (future (while true
+                   (Thread/sleep 1000)
+                   (swap! counter dec))))
 
 (defn duration []
   (let [this-time (System/currentTimeMillis)
@@ -13,26 +23,26 @@
       (println (str duration "msecs for 100 requests")))
     (reset! last-time this-time)))
 
+(defn app [req]
+    ;(println (mod @counter 100))
+    (when (== 0 (mod @counter 100))
+      (duration))
+    (swap! counter inc)
+    {:status 200 :body (str "count CR-EAM :-)   " @counter) :headers {}}) ;; a really basic handler
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "$PORT = " (System/getenv "PORT") ", default = 8080")
+(defn start-server []
+  (reset! server
+          (jetty/run-jetty (fn [req](app req))
+                           {:port  (Integer/parseInt (or (System/getenv "PORT") "80")) ;; listen on port 3001
+                            :join? false})))
 
-  (future (while true
-            (Thread/sleep 1000)
-            (swap! counter dec)))
+(defn stop-server []
+  (when-let [s @server]
+    (.stop s)
+    (reset! server nil)))
 
-  (jetty/run-jetty (fn [req]
-                     ;(println (mod @counter 100))
-                     (when (== 0 (mod @counter 100))
-                       (duration))
-                     (swap! counter inc)
-                     ;;(println @counter)
-                     {:status 200 :body (str "count CR-EAM :-)   " @counter) :headers {}}) ;; a really basic handler
-                   {:port  (Integer/parseInt (or (System/getenv "PORT") "80")) ;; listen on port 3001
-                    :join? false})
+(defn -main [& args]
+  (println "$PORT = " (System/getenv "PORT") ", default = 80")
+  (start-server)
   (println "main thread finished"))
-
-;; don't block the main thread)
 
