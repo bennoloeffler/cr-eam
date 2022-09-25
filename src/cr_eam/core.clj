12e24c813a89a4a09d848148d4c49365acbdc9ce
bennoloeffler <benno.loeffler@gmx.de>
2022-09-25 20:30:16 +0200

    middleware for param parsing

--- src/cr_eam/core.clj
+++ src/cr_eam/core.clj
@@ -1,57 +1,47 @@
 (ns cr-eam.core
   (:require [ring.adapter.jetty :as jetty]
-            [compojure.core :refer :all]
+            [compojure.core :as comp]
             [compojure.route :as route]
-            [clojure.pprint :as pprint])
+            [clojure.pprint :as pprint]
+            [ring.middleware.params :refer [wrap-params]]
+            [cr-eam.counter :as c])
   (:gen-class))
 
 ;; https://ericnormand.me/guide/clojure-web-tutorial
 
-(def server (atom nil))
-
-(def counter (atom 0N))
-(def last-time (atom (System/currentTimeMillis)))
-
-(defn duration []
-  (let [this-time (System/currentTimeMillis)
-        duration  (as-> @last-time $
-                        (- this-time $)
-                        (long $))
-        result    (str duration " msecs")]
-    (reset! last-time this-time)
-    result))
+(defonce server (atom nil))
 
 
-(defn inc-counter []
-  (swap! counter inc)
-  ;;(println @counter)
-  (let [timing (if (== 0 (mod @counter 100))
-                 (duration)
-                 "")
-        _      (when (not= timing "") (println "t: " timing " msecs for 100 requests"))
-        body   (str (format "counter: %,d" (biginteger @counter)) " " timing)]
-
-    {:status 200 :body body :headers {}}))
+(defn get-query-string [req]
+  (let [query-str (or (:query-string req) "no query-string found")
+        params (or (:params req) "no params found")]
+    (str "query-string: " query-str "<br>params: " params)))
 
 
-(defroutes app
-           (GET "/" [] {:status 200
-                        :body "<h1>Homepage</h1>
+(comp/defroutes routes
+                (comp/GET "/" [] {:status  200
+                                  :body    "<h1>Homepage</h1>
                                 <ul>
                                     <li><a href=\"/echo\">Echo request</a></li>
                                     <li><a href=\"/counter\">Counter</a></li>
                                     <li><a href=\"/about\">About</a></li>
-                                </ul>"
-                        :headers {"Content-Type" "text/html; charset=UTF-8"}})
+                                    <li><a href=\"/query?name=Sabine\">Query</a></li>\n                                </ul>"
+                                  :headers {"Content-Type" "text/html; charset=UTF-8"}})
 
-           (ANY "/echo" req {:status  200
-                             :body    (with-out-str (pprint/pprint req))
-                             :headers {"Content-Type" "text/plain"}})
-           (GET "/counter" [] (inc-counter))
-           (GET "/about" [] "<h1>Bennos kleine Seite...</h1>")
-           (route/not-found {:status 404
-                             :body "<h2>Page not found. Hmmm.....</h2>"
-                             :headers {"Content-Type" "text/html"}}))
+                (comp/ANY "/echo" req {:status  200
+                                       :body    (with-out-str (pprint/pprint req))
+                                       :headers {"Content-Type" "text/plain"}})
+                (comp/GET "/counter" [] (c/inc-counter))
+                (comp/GET "/about" [] "<h1>Bennos kleine Seite...</h1>")
+                (comp/GET "/query" [] get-query-string)
+                (route/not-found {:status  404
+                                  :body    "<h2>Page not found. Hmmm.....</h2>"
+                                  :headers {"Content-Type" "text/html"}}))
+
+
+;; enroll :query-string by middleware wrap-params
+(def app (wrap-params routes))
+
 
 (defn start-server []
   (reset! server
@@ -67,10 +57,12 @@
         (reset! server nil)
         (println "NOT STOPPED!")))))
 
+
 (defn -main [& args]
   (println "$PORT = " (System/getenv "PORT") ", default = 80")
   (start-server)
   (println "main thread finished"))
+
 
 (comment
   (start-server))
---
+++ src/cr_eam/counter.clj
@@ -0,0 +1,26 @@
+(ns cr-eam.counter)
+
+(def counter (atom 0N))
+(def last-time (atom (System/currentTimeMillis)))
+
+
+(defn duration []
+  (let [this-time (System/currentTimeMillis)
+        duration  (as-> @last-time $
+                        (- this-time $)
+                        (long $))
+        result    (str duration " msecs")]
+    (reset! last-time this-time)
+    result))
+
+
+(defn inc-counter []
+  (swap! counter inc)
+  ;;(println @counter)
+  (let [timing (if (== 0 (mod @counter 100))
+                 (duration)
+                 "")
+        _      (when (not= timing "") (println "t: " timing " msecs for 100 requests"))
+        body   (str (format "counter: %,d" (biginteger @counter)) " " timing)]
+
+    {:status 200 :body body :headers {}}))
