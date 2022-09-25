b9699efd0c78ed9b1723053af5d3c0e57cdd4717
bennoloeffler <benno.loeffler@gmx.de>
2022-09-25 14:19:58 +0200

    initial

--- README.md
+++ README.md
@@ -1,36 +1,20 @@
 # cr-eam
 
-FIXME: description
+Easy CRM based on Evernote. 
+CRM, _E_vernote, _EA_sy --> cr-eeam --> cr-eam  
 
 ## Installation
 
-Download from http://example.com/FIXME.
+no
 
 ## Usage
 
-FIXME: explanation
+https://cr-eam.herokuapp.com/
 
-    $ java -jar cr-eam-0.1.0-standalone.jar [args]
-
-## Options
-
-FIXME: listing of options this app accepts.
-
-## Examples
-
-...
-
-### Bugs
-
-...
-
-### Any Other Sections
-### That You Think
-### Might be Useful
 
 ## License
 
-Copyright © 2022 FIXME
+Copyright © 2022 Benno Löffler
 
 This program and the accompanying materials are made available under the
 terms of the Eclipse Public License 2.0 which is available at
--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject cr-eam "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Easy CRM based on Evernote."
+  :url "https://github.com/bennoloeffler/cr-eam"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring/ring-core "1.9.1"]
-                 [ring/ring-jetty-adapter "1.9.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.9.6"]
+                 [ring/ring-jetty-adapter "1.9.6"]]
   :min-lein-version "2.0.0"
   :main ^:skip-aot cr-eam.core
   :target-path "target/%s"
