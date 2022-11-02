32f5a4e3666ba8c499a6b3fe6670f14c8ccf6c47
bennoloeffler <benno.loeffler@gmx.de>
2022-11-02 16:30:02 +0100

    without my-config.edn

--- src/cr_eam/core.clj
+++ src/cr_eam/core.clj
@@ -103,7 +103,7 @@
                                        :headers {"Content-Type" "text/html"}})
 
                 (comp/GET "/jdbc-url" [] {:status 200}
-                                         :body (wrap-hiccup [:pre (with-out-str  (config/config))])
+                                         :body (wrap-hiccup [:pre (with-out-str  (println (config/config)))])
                                          :headers {"Content-Type" "text/html"})
 
 
